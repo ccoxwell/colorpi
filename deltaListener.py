ef98552cbed19811e3d2c8da6a2ec202801ca2a6
Carrie Coxwell <carrie.coxwell@gmail.com>
2019-07-06 12:18:50 -0500

    got the args to execl wrong

--- deltaListener.py
+++ deltaListener.py
@@ -43,7 +43,7 @@ class shadowCallbackContainer:
         print(r, g, b)
         # rgbexp.main(r, g, b)
         cmd = "python rgbexp.py %s %s %s" % (r, g, b)
-        os.execl(cmd)
+        os.execl("python", "rgbexp.py", r, g, b)
         print("Request to update the reported state...")
         newPayload = '{"state":{"reported":' + deltaMessage + '}}'
         self.deviceShadowInstance.shadowUpdate(newPayload, None, 5)
